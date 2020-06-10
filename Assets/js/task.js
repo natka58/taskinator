980475da151dfa0237c5199d5a28789a66e62f01
Natalia Teaca <lilikoy@sbcglobal.net>
2020-06-10 00:26:11 -0700

    task-handler

---
+++ Assets/js/task.js
@@ -0,0 +1,25 @@
+//Botton element
+var formEl = document.querySelector("#task-form");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+//Task Item element
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    console.dir(taskNameInput);
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
+// create list item
+var listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
+// create div to hold task info and add to list item
+var taskInfoEl = document.createElement("div");
+// add HTML content to div
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+listItemEl.appendChild(taskInfoEl);
+
+   tasksToDoEl.appendChild(listItemEl); 
+  };
+  formEl.addEventListener("submit", createTaskHandler);
+  
+
+
